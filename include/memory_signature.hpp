b583e6d732eeabc1f7897f27ded89e4a8da494ea
Justas Masiulis <Justas.masiulis@yahoo.com>
2017-09-10 14:51:13 +0300

    added copy constructor and copy assignment operator

--- include/memory_signature.hpp
+++ include/memory_signature.hpp
@@ -22,23 +22,58 @@
 
 namespace jm {
 
-    class signature {
+    class memory_signature {
         std::unique_ptr<unsigned char[]> _pattern;
         unsigned char                    *_end;
         unsigned char                    _wildcard;
 
+        std::size_t size() const noexcept { return _end - _pattern.get(); }
+
     public:
         /// \brief Construct a new signature that is empty
-        explicit signature() noexcept
+        explicit constexpr memory_signature() noexcept
                 : _pattern(nullptr)
                 , _end(nullptr)
-                , _wildcard(0)
-        {}
+                , _wildcard(0) {}
 
         /// \brief Destroys the stored signature
-        ~signature() noexcept = default;
+        ~memory_signature() noexcept = default;
 
-        
+        memory_signature(const memory_signature &other)
+                : _wildcard(other._wildcard)
+        {
+            auto new_pat = std::make_unique<unsigned char[]>(other.size());
+            std::copy(other._pattern.get(), other._end, new_pat.get());
+            _pattern = std::move(new_pat);
+            _end     = _pattern.get() + other.size();
+        }
+
+        memory_signature& operator= (const memory_signature &other)
+        {
+            auto new_pat = std::make_unique<unsigned char[]>(other.size());
+            std::copy(other._pattern.get(), other._end, new_pat.get());
+            _pattern  = std::move(new_pat);
+            _end      = _pattern.get() + other.size();
+            _wildcard = other._wildcard;
+            return *this;
+        }
+
+
+        memory_signature(const std::string &pattern, unsigned char wildcard)
+                : _pattern(std::make_unique<unsigned char[]>(pattern.size()))
+                , _end(_pattern.get() + pattern.size())
+                , _wildcard(wildcard)
+        {
+            std::copy(std::begin(pattern), std::end(pattern), _pattern.get());
+        }
+
+        memory_signature(std::initializer_list<unsigned char> pattern, unsigned char wildcard)
+                : _pattern(std::make_unique<unsigned char[]>(pattern.size()))
+                , _end(_pattern.get() + pattern.size())
+                , _wildcard(wildcard)
+        {
+            std::copy(std::begin(pattern), std::end(pattern), _pattern.get());
+        }
 
 
         /// \brief Searches for first occurrence of stored signature in the range [first, last - signature_length).
@@ -49,7 +84,7 @@ namespace jm {
     };
 
     template<class ForwardIt>
-    ForwardIt signature::find(ForwardIt first, ForwardIt last) const
+    ForwardIt memory_signature::find(ForwardIt first, ForwardIt last) const
     {
         if (_pattern.get() == _end)
             return last;
